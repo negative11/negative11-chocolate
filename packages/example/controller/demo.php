7e5cab498ee10375575044c7cb514e6d5ebfa44b
John Squibb <admin@negative11.com>
2012-06-26 07:06:37 -0700

    Added a couple of example methods to example demo controller

--- packages/example/controller/demo.php
+++ packages/example/controller/demo.php
@@ -8,8 +8,32 @@ namespace controller;
 
 class Demo extends Core
 {
+	/**
+	 * Demo lander. Displays a quick greeting and template example. 
+	 */
 	public function main()
 	{
+		// Set the template for display.
 		$this->setTemplate('demo');
+		
+		// We can assign variables to the template like this.
+		$this->template->message = "This message is added by assignment from the controller.";
+	}
+	
+	/**
+	 * Shows how to work with basic objects you create.
+	 */
+	public function objects()
+	{
+		// Try to load a model object.
+		$model = new \model\User;
+	}
+	
+	/**
+	 * Demonstrates use of the error debugger. 
+	 */
+	public function error()
+	{
+		throw new \Exception ('We threw this error on purpose.');
 	}
 }
