1ee04645668dd45621bea9cf11e8231942a84208
John Squibb <admin@negative11.com>
2012-09-23 08:09:17 -0700

    sending missed session config

---
+++ system/config/session.php
@@ -0,0 +1,14 @@
+<?php
+$config = array
+(
+	// Expiration time in seconds.
+	'lifetime' => 7200,
+	'name' => 'phpsession',
+	'cookie' => array 
+	(
+		'path' => NULL,
+		'domain' => NULL,
+		'secure' => FALSE,
+		'httponly' => FALSE,
+	)
+);
