7d2834123c58b2531996a307c261ebfa316a3d95
John Squibb <admin@johnsquibb.com>
2013-03-12 05:58:49 -0700

    Updated copyright date, added link to github issues page, adjusted capitalization on some words.

--- system/template/hello.php
+++ system/template/hello.php
@@ -28,12 +28,12 @@
 		<h1>Welcome to the negative(-11) PHP Framework!</h1>
 		<p>If you can see this page, then everything is working correctly!</p>
 		<h2>Learn More</h2>
-		<p>Check us out over at <a href="https://github.com/negative11/negative11-chocolate/">github</a>. 
+		<p>Check us out over at <a href="https://github.com/negative11/negative11-chocolate/">GitHub</a>. 
 			If you have never used this version of the framework before, be sure to read the <a href="https://github.com/negative11/negative11-chocolate/wiki/">wiki</a>.
 		</p>
 		<h2>About</h2>
 		<p>
-		&copy; Copyright 2012 negative(-11) PHP Framework
+		&copy; Copyright 2013 negative(-11) PHP Framework
         <br>
         Created and maintained by John Squibb
         <br>
@@ -41,7 +41,7 @@
 		</p>
 		<h2>Participate</h2>
 		<p>Keep up with the latest framework news via <a href="http://strem.in/profile/negative11">strem.in</a>. You are encouraged to join in on 
-		the discussion and provide feedback or bug reports. If you would like to submit a feature or modification, please contact the <a href="http://johnsquibb.com/contact">maintainer</a></p>
+		the discussion and provide feedback or bug reports. Please report all issues on <a href="https://github.com/negative11/negative11-chocolate/issues">GitHub</a>.</p>
 		<h2>Open Source</h2>
 		<p>This framework is dual-licensed under the GPLv3 and/or MIT licenses per your requirements. You may modify it and redistribute it as you wish.</p>
 		<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
