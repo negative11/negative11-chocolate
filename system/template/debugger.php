f42ee8b8a15cf95105b30d95088f20feeb6c547d
John Squibb <admin@johnsquibb.com>
2013-08-31 04:31:23 -0700

    Modified display of bugger. Changed all PHP short tags to full tags for best support.

--- system/template/debugger.php
+++ system/template/debugger.php
@@ -18,26 +18,34 @@
 	<body>
 		<div class="main">
 			<h1>Framework Error</h1>
-			<?php
-			// Shield error output in production mode.
-			if (IN_PRODUCTION)
-			{
-				?>
+			<?php	if (IN_PRODUCTION): // Hide errors in production mode ?>
 				<p style="color:#bb0000;font-weight:bold;">An error has occurred. Please contact the site administrator for further assistance.</p>
-				<?php
-				exit;
-			}
-			?>			
-			<p>Message: <strong><?=$message;?></strong>
-			<br/>
-			Line Number: <?=$line_number;?>
-			<br/>
-			File Name: <?=$file_name;?>
-			<br/>
-			Type: <?=$error_type;?>
-			</p>
-			<h2>Backtrace</h2>
+      <? else: ?>	
+			<p style="color:#bb0000;font-weight:bold;"><strong><?php echo $message;?></strong></p>
+			<p><strong>Line Number: </strong><?php echo $line_number;?></p>
+			<p><strong>File Name: </strong><?php echo $file_name;?></p>
+      <p><strong>Type: </strong><?php echo $error_type;?></p>
+			<h2>Stack Trace</h2>
+        <p>Displayed in reverse order.</p>
+        <?php if (is_array($backtrace)): ?>
+          <?php 
+            $styles = array('background-color:#fff', 'background-color:#eee');
+            $current = 0;
+          ?>
+          <?php foreach ($backtrace as $item): ?>
+            <?php $current = $current == 2 ? 0 : $current; ?>
+            <table style="<?php echo $styles[$current++] ?>;width:100%;overflow:auto;border-spacing:0;border-collapse:collapse;">
+            <?php foreach ($item as $key => $value): ?>
+              <tr >
+                <td style="text-align:left;width:25%;border:1px solid lightgray;padding:10px;vertical-align:top;"><?php echo ucfirst($key) ?></td>
+                <td style="text-align:left;border:1px solid lightgray;padding:10px;"><pre><?php print_r($value) ?></pre></td>
+              </tr>
+            <?php endforeach; ?>
+            </table>
+            <br>
+          <?php endforeach ?>
+        <?php endif ?>
+      <?php endif ?>
 		</div>
-		<?Core::dump($backtrace);?>
 	</body>
 </html>
